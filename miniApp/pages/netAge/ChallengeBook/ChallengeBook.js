50fcf11d4fc631a9e3bce48dbaa804776b1ac7af
ren <842124678@qq.com>
2019-01-02 22:19:45 +0800

    11

--- miniApp/pages/netAge/ChallengeBook/ChallengeBook.js
+++ miniApp/pages/netAge/ChallengeBook/ChallengeBook.js
@@ -6,23 +6,57 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    friendTel:13333333333,
+    myTel: 13812113211,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      localhost: app.globalData.getImage
+    })
   },
-
+  
+  /**
+   * 输入我的号码
+   */
+  myTel:function(e){
+    this.setData({
+      myTel:e.detail.value
+    })
+  },
+  /**
+   * 换个号码
+   */
+  cutTel:function(){
+    
+  },
+  /**
+   * 接受挑战
+   */
+  accep:function(){
+    console.log("接受挑战")
+  },
+  /**
+   * 领取礼品
+   */
+  getPrize:function(){
+    console.log("领取礼品")
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
 
   },
-
+  /**
+   * 点击关闭按钮
+   */
+  offBook: () => {
+    console.log("点击关闭按钮")
+  },
   /**
    * 生命周期函数--监听页面显示
    */
